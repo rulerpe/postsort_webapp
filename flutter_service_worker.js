57f0287866b800cc38e19e38a4da2f3aa51ad5b1
rulerpe <neetisthebest@gmail.com>
2024-02-12 09:45:41 -0800

    updated font and styles

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -25,9 +25,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "e01eba1b9d5ce28e93a201346bcfc75b
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "2761b0d1629b6f997f987fc3da3cf156",
-"/": "2761b0d1629b6f997f987fc3da3cf156",
-"main.dart.js": "66dadc41db77b51d22f5bcb5bd6b76f8",
+"index.html": "0053761badfa90aaea23410f311c2228",
+"/": "0053761badfa90aaea23410f311c2228",
+"main.dart.js": "9e4e47bee017151680f008c74ac072b7",
 "manifest.json": "dac4c3dde0e4397fc85466e90d366fcf",
 "version.json": "0f228ac3af8f0e20bb9d62026d30418d"};
 // The application shell files that are downloaded before a service worker can
