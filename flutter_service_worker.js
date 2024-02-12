afef533d7e5eee171d606b93620d4f6dd584c230
rulerpe <neetisthebest@gmail.com>
2024-02-12 11:54:17 -0800

    global state

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -25,9 +25,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "e01eba1b9d5ce28e93a201346bcfc75b
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "ff6e344768139239060347adf1664c52",
-"/": "ff6e344768139239060347adf1664c52",
-"main.dart.js": "00456d803032c4243bc20a6a9a9ad350",
+"index.html": "3589966f401f5c16c4a2f1d48e8c1eac",
+"/": "3589966f401f5c16c4a2f1d48e8c1eac",
+"main.dart.js": "4b0ead7666002d2b9d5439ae0a325eff",
 "manifest.json": "dac4c3dde0e4397fc85466e90d366fcf",
 "version.json": "0f228ac3af8f0e20bb9d62026d30418d"};
 // The application shell files that are downloaded before a service worker can
